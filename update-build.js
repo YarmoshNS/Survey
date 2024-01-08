6cf206df2cd8fa75a26b1e3d16fea37d3eb36a10
YarmoshNS <70883789+YarmoshNS@users.noreply.github.com>
2024-01-08 14:53:32 +0300

    Add files via upload

---
+++ update-build.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const buildDate = new Date().getTime();
+
+const jsonData = {
+  buildDate: buildDate,
+};
+
+const jsonContent = JSON.stringify(jsonData);
+
+fs.writeFile("./public/meta.json", jsonContent, "utf8", function (error) {
+  if (error) {
+    console.log("An error occurred while saving build date and time to meta.json");
+    return console.log(error);
+  }
+
+  console.log("Latest build date and time updated in meta.json file");
+});
